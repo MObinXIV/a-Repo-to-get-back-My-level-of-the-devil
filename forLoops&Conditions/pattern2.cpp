22f6f43e569b4d2e85d44dae8bc40643e948d8dc
MObinXIV <mobinxiv@gmail.com>
2024-12-20 01:44:44 +0200

    add the right rectangle

---
+++ forLoops&Conditions/pattern2.cpp
@@ -0,0 +1,26 @@
+// The pattern
+
+/*
+
+*
+***
+****
+*****
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+   int height;
+   cin>>height;
+   for(int i=0 ; i<height;i++)
+   {
+      for(int j=0;j<=i;j++)
+      {
+         cout<<"*";
+      }
+      cout<<endl;
+   }
+
+}
