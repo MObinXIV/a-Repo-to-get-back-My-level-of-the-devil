ea739c023bb58fd5c96b8e346b49579aaf9f4ff7
MObinXIV <mobinxiv@gmail.com>
2024-12-19 15:35:29 +0200

    new changes

--- lol.cpp
+++ lol.cpp
@@ -1,14 +1,15 @@
-
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
-void main()
+int main()
 {
-    cout<<"hello world"<<endl;
+   cout<<"hello world"<<endl;
 }
-/*end
--lil wins
-- atlete wins
-- Baraca wins
-- bilbo wins
 
+/*>>
+the linker:
+
+Resolves external references: If your code uses functions or data defined in other files (like libraries), the linker finds those definitions and connects them correctly.
+Allocates memory: It assigns memory addresses to different parts of the program (code, data, etc.).
+Creates the final executable: It combines all the object files into a single executable file that can be run on the computer.
+This process ensures that all the different parts of your program work together correctly and that the program can run independently.
 */
