16c31baca951b92b8a8f75b536b0d86a84d87c38
MObinXIV <mobinxiv@gmail.com>
2024-12-27 07:32:37 +0200

    something

--- Math/reverseNum.cpp
+++ Math/reverseNum.cpp
@@ -5,18 +5,16 @@
  int main() 
 
  {     
-    
-    int n ;
-    cin>>n;
-    int rev=0;
-    while(n!=0)
+    int x = 123;
+    int rev = 0;
+    while(x!=0)
     {
-        int pop=n%10;
-        n/=10;
-        if(rev>INT_MAX/10 || (rev==INT_MAX/10 && pop>7))return 0; // Check for overflow
-        if(rev<INT_MIN/10 || (rev==INT_MIN/10 && pop<-8))return 0;
-        rev=rev*10+pop;
+        int pop = x%10;
+        x/=10;
+        if(rev > INT_MAX/10 || (rev == INT_MAX/10 && pop > 7)) return 0;
+        if(rev < INT_MIN/10 || (rev == INT_MIN/10 && pop < -8)) return 0;
+        rev = rev*10 + pop;
     }
-    cout<<rev<<endl;  
+    cout<<rev;
  }
  
