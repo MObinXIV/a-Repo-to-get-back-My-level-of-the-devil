12336b08e6df729519189f03126cdcb5de68f552
MObinXIV <mobinxiv@gmail.com>
2024-12-31 20:26:32 +0200

    add count pattern and change hireracy

---
+++ Recursion/Backtracking/basicPatterns/countPattern/countImportantPattern.md
@@ -0,0 +1,10 @@
+# Counting 
+
+`int f(){
+    //base case
+return 1 -> if condition satisfied
+return 0 -> condition not satisfied
+l=f()
+r=f()
+return l+r;
+}`
---
+++ Recursion/Backtracking/basicPatterns/countPattern/countSumEqualSum.cpp
@@ -0,0 +1,40 @@
+// Subset Sum Problem
+#include<bits/stdc++.h> 
+ using namespace std;
+/*
+Modify only one answer
+*/
+int  subsetSum(int i, vector<int>&nums,int n,int s,int sum){
+    
+    // Whenever I reach the end of the array, I will push the data structure into the answer
+    if(i==n )
+    {
+        if (s==sum)
+       {
+       return 1;
+       }
+       //condition not satisfied
+        return 0;
+    }
+    // Pick the element
+    s+=nums[i];
+    int l=subsetSum(i+1,nums,n,s,sum);
+    // Do not pick the element in the array subsequence
+    s-=nums[i];
+    int r=subsetSum(i+1,nums,n,s,sum);
+    return l+r;
+ }
+
+
+ int subsets(vector<int>& nums,int s) {
+   
+    return subsetSum(0,nums,nums.size(),0,s);
+    
+    }
+ int main() 
+ {    
+    vector<int> nums={1,2,1};
+    int s=3;
+    cout<<subsets(nums,s);
+    return 0;
+ }
