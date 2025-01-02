7496ed87345a9ab4a7923268f4fc7ffe953ac603
MObinXIV <mobinxiv@gmail.com>
2025-01-02 14:29:48 +0200

    add the O(n) soluion using hashing technique

--- Hash/2Sum.cpp
+++ Hash/2Sum.cpp
@@ -1,8 +1,7 @@
 //https://leetcode.com/problems/two-sum/?envType=problem-list-v2&envId=hash-table
 #include<bits/stdc++.h> 
  using namespace std;
-class Solution {
-public:
+ // O(n^2) solution
     vector<int> twoSum(vector<int>& nums, int target) {
         int sum = 0;
         vector<int> indices(2);
@@ -19,13 +18,23 @@ public:
         }
         return indices;
     }
-};
+// O(n) solution
+vector<int> twoSum(vector<int>& nums, int target) {
+      unordered_map<int,int>mp;
+      for(int i=0;i<nums.size();i++){
+         if(mp.find(target-nums[i])!=mp.end())
+         {
+            return {mp[target-nums[i]],i};
+         }
+         mp[nums[i]]=i;
+      }
+    return {-1,-1};
+    }
  int main() 
  {    
-      Solution s;
-      vector<int> nums = {2,7,11,15};
-      int target = 9;
-      vector<int> result = s.twoSum(nums, target);
+      vector<int> nums = {3,2,4};
+      int target =6;
+      vector<int> result = twoSum(nums, target);
       for(int i = 0; i < result.size(); i++){
          cout<<result[i]<<" ";
       }
