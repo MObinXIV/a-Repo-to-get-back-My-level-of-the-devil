386f52a7938830ced2e2150a6ad9e375344725b2
MobinXiv <mobinxiv@gmail.com>
2025-01-21 08:22:59 +0200

    add the reverseLinkedList recursive function

--- DataStructure/LinkedList/problems/reverseLinkedList.cpp
+++ DataStructure/LinkedList/problems/reverseLinkedList.cpp
@@ -23,3 +23,22 @@ using namespace std;
 
     }
 
+    // Calling the recursion function till I reach one node or Null 
+// storing it's value to return it 
+// keep reversing the the links by making the front guy (the node of the previous call ) points to the currnet guy
+// & making current guy points to null
+ListNode* reverseList(ListNode* head) {
+    // get my last node as the new Head
+       if(head==NULL||head->next==NULL) return head;
+
+        ListNode*newHead=reverseList(head->next);
+        ListNode*front=head->next; // get the previous guy node 
+        // linking it reverserly to the current node 
+        front->next=head;
+        // making the currnet guy points to null as we go to end will points to null by default 
+        head->next=NULL;
+
+        // return the newHead node 
+        return newHead;
+    }
+
