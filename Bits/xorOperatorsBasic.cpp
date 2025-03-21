dfe0927d679c77ad5ae6e21a14f64f3a66d41fcd
MObinXIV <mobinxiv@gmail.com>
2025-03-21 23:01:20 +0200

    add basic operations

---
+++ Bits/xorOperatorsBasic.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*
+
+! = Not 
+~ = complement
+& = AND 
+| = OR 
+^ = XOR
+<< = Shift left 
+>> = Shift right
+*/
+
+int main()
+{   
+   int x = 7 ;
+   int y = 8;
+   cout<<(x^y)<<endl;
+}
+    
