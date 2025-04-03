b36a1ea493c52610d531ef26a770bd099c44094f
MObinXIV <mobinxiv@gmail.com>
2025-04-03 02:28:24 +0200

    add check if the bt is balanced

---
+++ DataStructure/Tree/BT/checkBalanced.cpp
@@ -0,0 +1,51 @@
+//https://leetcode.com/problems/balanced-binary-tree/description/
+#include<bits/stdc++.h> 
+ using namespace std;
+ struct TreeNode{
+    int val;
+    TreeNode*left;
+    TreeNode*right;
+    TreeNode(int val){
+        this->val=val;
+        left=right=NULL;
+    }
+ };
+ /*
+Whenever the tree got unblancing , we stop traverse 
+O(n)
+ */
+ int maxDepth(TreeNode* root) {
+        
+   if (root ==NULL)
+       return 0;
+       int l=maxDepth(root->left);
+      //  cout<<"left is "<<l<<endl;
+       if(l==-1) return -1;
+       int r=maxDepth(root->right);
+       cout<<"right is "<<r<<endl;
+      if(r==-1) return -1;
+      // cout<<"absloute value at " <<root->val<<" is"<<" "<<abs(l-r)<<endl;
+      // check to stop before we reach to the other one
+      if(abs(l-r)>1) return -1 ;
+       return max(l,r)+1;
+   
+   }
+ bool isBalanced(TreeNode* root) {
+  return maxDepth(root)!=-1;
+}
+ int main() 
+ {    
+    TreeNode*root=new TreeNode(1);
+    root->left= new TreeNode(2);
+    root->right= new TreeNode(2);
+    
+    root->left->left= new TreeNode(2);
+    root->left->right= new TreeNode(3);
+    root->left->left->left= new TreeNode(3);
+    root->left->left->right= new TreeNode(3);
+    root->left->left->right->left= new TreeNode(4);
+    root->left->left->right->right= new TreeNode(4);
+
+
+    cout<<"The tree is "<<isBalanced(root)<<" balanced"<<endl;  
+ }
