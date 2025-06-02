2ce44d1c2a498be487fa300ac661b3925540d22b
MObinXIV <mobinxiv@gmail.com>
2025-06-02 19:26:09 +0300

    longestPalindormicSub

---
+++ Algorithms/Dp/problems/string/longestPalindormicSubsequence.cpp
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/longest-palindromic-subsequence/description/
+#include<bits/stdc++.h>
+using namespace std;
+// longest common subsequence between the string & it's reversed one 
+// any thing is palindrome if it's matched it's reversed one 
+int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.size();
+        int m = text2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-0));
+        for(int i=0;i<=n;i++) dp[i][0]=0;
+        for(int i=0;i<=m;i++)dp[0][i] = 0;
+        for(int ind1=1;ind1<=n;ind1++){
+        for(int ind2=1;ind2<=m;ind2++){
+            if(text1[ind1-1]==text2[ind2-1])
+                dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1];
+            else
+                dp[ind1][ind2] = 0 + max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
+        }
+    }
+    
+    return dp[n][m];
+    }
+int longestPalindromeSubseq(string s) {
+        string t=s;
+        reverse(s.begin(),s.end());
+        return longestCommonSubsequence(s,t);
+    }
