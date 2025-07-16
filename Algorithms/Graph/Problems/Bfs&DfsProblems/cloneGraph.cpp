cd14d180d9de177e86541cea9dd96ae25b05b4a4
MObinXIV <mobinxiv@gmail.com>
2025-07-16 06:45:48 +0300

    clone graph problem

---
+++ Algorithms/Graph/Problems/Bfs&DfsProblems/cloneGraph.cpp
@@ -0,0 +1,41 @@
+//https://leetcode.com/problems/clone-graph/description/
+#include<bits/stdc++.h>
+using namespace std;
+   class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+ Node* cloneGraph(Node* node) {
+        if (!node) return nullptr;
+
+        unordered_map<Node*, Node*> visited;
+        return dfs(node, visited);
+    }
+
+    Node* dfs(Node* node, unordered_map<Node*, Node*>& visited) {
+        if (visited.find(node) != visited.end())
+            return visited[node];
+
+        Node* clone = new Node(node->val);
+        visited[node] = clone;
+
+        for (Node* neighbor : node->neighbors) {
+            clone->neighbors.push_back(dfs(neighbor, visited));
+        }
+
+        return clone;
+    }
