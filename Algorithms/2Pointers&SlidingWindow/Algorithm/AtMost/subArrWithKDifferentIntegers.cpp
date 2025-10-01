7de56a597b8cb9f6d9f74eb27f8d676c3df3294c
MObinXIV <mobinxiv@gmail.com>
2025-10-01 19:28:47 +0300

    countVowel

--- Algorithms/2Pointers&SlidingWindow/Algorithm/AtMost/subArrWithKDifferentIntegers.cpp
+++ Algorithms/2Pointers&SlidingWindow/Algorithm/AtMost/subArrWithKDifferentIntegers.cpp
@@ -1,18 +1,29 @@
 //https://leetcode.com/problems/subarrays-with-k-different-integers/
 #include<bits/stdc++.h> 
 using namespace std;
+/**
+ * --- Approach ---
+ * we got all the subarrays <=k
+ * & <k which is mostly the k-1 
+ * so by subtracting them we got the subArrs with k size 
+ */
 int atMost(vector<int>&nums,int k){
   int n = nums.size();
   int l=0,r=0,cnt=0 , uniqueElements=0;
   unordered_map<int,int>mp;
   while(r<n){
-      mp[nums[r]]++;
+      mp[nums[r]]++;// keep increasing
+      // once I exceed k 
       while(mp.size()>k)
       {
+        // decrease the count of the element in the map
         mp[nums[l]]--;
+        // in case it's count is 0 , we erase it
         if(mp[nums[l]]==0) mp.erase(nums[l]);
+        // keep shrinking the window
         l++;
       }
+      // add the window size
       cnt+=(r-l+1);
       r++;
   }
@@ -20,6 +31,7 @@ int atMost(vector<int>&nums,int k){
   return cnt;
 }
 int subarraysWithKDistinct(vector<int>& nums, int k) {
+        // subtract <=k - <k = k distinct guys
         return atMost(nums,k)-atMost(nums,k-1);
 }
 int main() 
